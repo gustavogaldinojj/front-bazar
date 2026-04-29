f5227dea5db6e991d0927566b3a8c1982b9d67df
gustavo <gustavogaldinojj@gmail.com>
2026-04-29 00:25:00 -0300

    Fix: Alterando cor background

--- src/components/auth/LoginBanner.vue
+++ src/components/auth/LoginBanner.vue
@@ -12,7 +12,7 @@
 <style scoped>
 .login-left {
   flex: 1;
-  background: linear-gradient(135deg, #1e3c72 0%, #2a5298 100%);
+  background: linear-gradient(135deg, #2a74fd 0%, #2a5298 100%);
   color: white;
   display: flex;
   flex-direction: column;
